29b24bfcf2f2da090e9589485fab9e0fcd326bc3
Bibek Rai <141400793+Bbkrae17@users.noreply.github.com>
2025-04-29 13:38:44 +0545

    Add files via upload

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,48 @@
+if(true) {
+    let a = 10
+    const b = 20
+}
+
+const myArray = [10,20,30]
+
+function one(){
+    const user = "Bibek"
+
+    function two(){
+        const website = "youtube"
+        console.log(user);
+    }
+    // console.log(website);
+
+    two()
+
+}
+
+// one()
+
+if (true){
+    const username = "Rai"
+    if(username === "Rai"){
+        const web = " youtube"
+        console.log(username + web)
+    }
+    // console.log(web);
+}
+
+// console.log(username);
+
+
+// +++++++++++++++++++++++ interesting ++++++++++++++++++++++++4
+
+function addone(val){
+    return val +1
+
+}
+console.log(addone(2))
+
+const addtwo = function(num){
+    return num + 2
+}
+
+console.log(addtwo(3))
+ 
